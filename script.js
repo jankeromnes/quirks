b53aa78f41d5b34351a9dbfb05e2362942dadb33
Jan Keromnes <janx@linux.com>
2014-01-23 14:57:42 +0100

    first quirk

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.querySelector('#clog').addEventListener('click', () => {
+  console.log('console.log');
+});
+
+document.querySelector('#cwarn').addEventListener('click', () => {
+  console.warn('console.warn');
+});
+
+document.querySelector('#cerr').addEventListener('click', () => {
+  console.error('console.error');
+});
+
+document.querySelector('#warn').addEventListener('click', () => {
+  // TODO
+});
+
+document.querySelector('#err').addEventListener('click', () => {
+  oops();
+});
+
+document.querySelector('#refl').addEventListener('click', () => {
+  // TODO
+});
